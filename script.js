66d9aadca39b147729a0e118c200c48ea0bee547
damkul <34838081+damkul@users.noreply.github.com>
2022-03-24 18:37:36 +0530

    added animantion to content-image section

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ timeline
 .to('.girl',3,{y:-200},'-=3')
 .fromTo('.bg1',3,{y:-50},{y:0, duration:3},'-=3')
 .to(".content",3,{top:"0%"},'-=3')
+.fromTo(".content-images",{opacity:0.2},{opacity:1,duration:1.5})
+.fromTo(".text",{opacity:0.1},{opacity:1,duration:1.5})
 
 let scene = new ScrollMagic.Scene({
     triggerElement: "section",
